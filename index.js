aacfdf7363f89b3b91681d10fcc1cff856e5343a
Evan Chernicky <echernicky@gmail.com>
2022-01-07 17:53:22 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+
+function writeCards() {
+
+    const messages = [];
+
+
+    for (let i= 0; i < names.length; i++) {
+
+        const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+
+        messages.push(message);
+    };
+
+    return messages;
+
+};
+
+writeCards(names);
+
+counter = 10;
+
+function countDown(counter) {
+
+    while (counter >= 0) {
+            console.log(counter--);
+        };
+
+};
+
+countDown(counter);
